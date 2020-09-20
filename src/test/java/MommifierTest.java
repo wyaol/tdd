e7321c791a2dc76c4cf24cf9908ee8380c73c056
wyao <289672494@qq.com>
2020-09-20 19:25:24 +0800

    [wang yao] 实现存在aeiou但不足0.3时原串返回

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -16,4 +16,17 @@ public class MommifierTest {
         // then
         assertEquals("wfrb", res);
     }
+
+    @Test
+    void should_return_self_with_have_aeiou_but_not_enough_0_3() {
+        // given
+        String str = "keppp";
+        Mommifier mommifier = new Mommifier();
+
+        // when
+        String res = mommifier.convert(str);
+
+        // then
+        assertEquals("keppp", res);
+    }
 }
